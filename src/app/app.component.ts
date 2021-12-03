82d662866fd7161faf0ca9f1077756c657eb1055
andersoncgiusti <anderson_cg12@hotmail.com>
2021-12-03 15:58:16 -0300

    create methods

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,72 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'calculator-angular';
+  title = 'Calculator Angular';
+
+  result: string = '';
+
+  one() {
+    this.result += '1';
+  }
+
+  two() {
+    this.result += '2';
+  }
+
+  three() {
+    this.result += '3';
+  }
+
+  for() {
+    this.result += '4';
+  }
+
+  five() {
+    this.result += '5';
+  }
+
+  six() {
+    this.result += '6';
+  }
+
+  seven() {
+    this.result += '7';
+  }
+
+  eight() {
+    this.result += '8';
+  }
+
+  nine() {
+    this.result += '9';
+  }
+
+  zero() {
+    this.result += '0';
+  }
+
+  sum() {
+    this.result += '+';
+  }
+
+  subtract() {
+    this.result += '-';
+  }
+
+  share() {
+    this.result += '/';
+  }
+
+  multiplication() {
+    this.result += '*';
+  }
+
+  clear() {
+    this.result = ' ';
+  }
+
+  results() {
+
+  }
+
 }
